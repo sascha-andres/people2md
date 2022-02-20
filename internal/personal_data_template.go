cc535d05d196a1fc1c214b3a5b389348ab4fa544
Sascha Andres <sascha.andres@infrontquant.com>
2022-02-20 18:54:56 +0100

    feat: add notes link to organizations
    Closes #6

--- internal/personal_data_template.go
+++ internal/personal_data_template.go
@@ -9,5 +9,6 @@ Notes: [[{{ (index .Names 0).DisplayName }} Notes|Notes]]
 {{ if gt (len .Birthdays) 0 }}{{ range .Birthdays }}Birthday: {{ if .Date }}{{ .Date.Year }}-{{ .Date.Month }}-{{ .Date.Day }}{{ else }}{{ if .Text }}{{ .Text }}{{end}}{{ end }} 
 {{end}}
 {{ end }}{{ else }}{{ if gt (len .Organizations) 0 }}# {{ (index .Organizations 0).Name }}
+Notes: [[{{ (index .Organizations 0).Name }} Notes|Notes]]
 
 {{ end }}{{ end }}`
