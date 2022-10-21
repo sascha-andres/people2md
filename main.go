3681b5341595e45dee781bcbec9733141e4cb00e
Sascha Andres <sascha.andres@outlook.com>
2022-10-21 13:09:34 +0200

    feat(#2): change import

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/sascha-andres/flag"
-	"livingit.de/code/people2md/internal"
+	"github.com/sascha-andres/people2md/internal"
 )
 
 var (
@@ -15,7 +15,7 @@ var (
 	pathForFiles      string
 	templateDirectory string
 	smsBackupFile     string
-  callBackupFile    string
+	callBackupFile    string
 )
 
 func init() {
@@ -29,7 +29,7 @@ func init() {
 	flag.StringVar(&pathForFiles, "output", ".", "store output in this directory")
 	flag.StringVar(&templateDirectory, "template-directory", "", "load templates from directcory")
 	flag.StringVar(&smsBackupFile, "sms", "", "path to sms backup file")
-  flag.StringVar(&callBackupFile, "calls", "", "path to call backup file")
+	flag.StringVar(&callBackupFile, "calls", "", "path to call backup file")
 }
 
 func main() {
@@ -59,7 +59,7 @@ func main() {
 		internal.WithPathToContacts(pathToContacts),
 		internal.WithSmsBackupFile(smsBackupFile),
 		internal.WithPathToGroups(pathToGroups),
-    internal.WithCallBackupFile(callBackupFile),
+		internal.WithCallBackupFile(callBackupFile),
 	)
 	if err != nil {
 		log.Fatal(err)
